b6bb85f8527e6680e531c5d3901ac55afa789bb6
Times0 <45049767+Times0@users.noreply.github.com>
2025-02-02 19:09:06 +0100

    Add training script with experiment tracking thanks to mlflow + challenger champion model chacking and updating automatically in registry. Fix requirements

---
+++ src/training.py
@@ -0,0 +1,65 @@
+import os
+from ultralytics import YOLO
+from pathlib import Path
+import mlflow
+from ultralytics import settings
+
+## IMPORTANT ##
+# Start mlflow before using `mlflow server --host 0.0.0.0 --port 5000`
+
+# Update a setting
+settings.update({"mlflow": True})
+
+EPOCHS = 4
+DATASET_PATH = "data/THE-dataset"
+
+
+if __name__ == "__main__":
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("YOLO_Training")
+
+    cwd = Path.cwd()
+    path = cwd.parent / "data" / "THE-dataset"
+
+    with mlflow.start_run():
+        model = YOLO("yolo11n")
+        model.train(
+            data=os.path.join(path, "yolo.yaml"),
+            epochs=EPOCHS,
+        )
+
+        mlflow.pytorch.log_model(model.model, "model")
+        current_metrics = mlflow.get_run(mlflow.active_run().info.run_id).data.metrics
+
+        # Check if challenger is better than champion
+        try:
+            champion_model = mlflow.pytorch.load_model("models:/Champion/latest")
+            champion_run = mlflow.get_run(
+                mlflow.pytorch.get_model_info("models:/Champion/latest").run_id
+            )
+            champion_metrics = champion_run.data.metrics
+
+            # Comparer les performances (exemple avec la métrique map50-95)
+            if current_metrics.get("metrics/map50-95", 0) > champion_metrics.get(
+                "metrics/map50-95", 0
+            ):
+                mlflow.register_model(
+                    f"runs:/{mlflow.active_run().info.run_id}/model",
+                    "YOLO-model",
+                    tags={"status": "Challenger"},
+                )
+            else:
+                mlflow.register_model(
+                    f"runs:/{mlflow.active_run().info.run_id}/model", "YOLO-model"
+                )
+        except mlflow.exceptions.MlflowException:
+            # Si pas de Champion, enregistrer comme Champion
+            model_version = mlflow.register_model(
+                f"runs:/{mlflow.active_run().info.run_id}/model",
+                "YOLO-model",
+                tags={"status": "Champion"},
+            )
+            client = mlflow.tracking.MlflowClient()
+            client.set_registered_model_alias(
+                "YOLO-model", "Champion", model_version.version
+            )
