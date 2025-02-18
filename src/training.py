0b086c0a7d748620a4955b88104a56c3999b9fa5
edo <coriandering@gmail.com>
2025-02-18 13:56:44 +0100

    refactor: refactored parametrization of training via env variables. docs: added docstring for training. chore: added type hints for training

--- src/training.py
+++ src/training.py
@@ -11,19 +11,32 @@ console = Console()
 load_dotenv('src/config/.env')
 
 class Train():
-    def __init__(self, model_name):
+    def __init__(self, model_name: str, data_yaml: str|os.PathLike, yolo_dir: str|os.PathLike, device: str|int) -> None:
+        """
+        Initializes the training object.
+
+        Args:
+            model_name (str): The name of the model.
+            data_yaml (str, os.PathLike): Path to the data yaml configuration file.
+            yolo_dir (str, os.PathLike): Directory where the model will be temporarily stored (overwriting past runs).
+            device (str, int): The device to run the model on. Either the device name or a number indicating which GPU to use
+        """
         self.model_name = model_name
-        self.data_yaml = os.path.join(os.getcwd(), os.getenv("DATA_YAML", "data/THE-dataset/yolo.yaml"))
-        self.yolo_dir = os.path.join(os.getcwd(),  os.getenv("YOLO_DIR_TMP", "tmp/yolo_runs"))
-        if os.getenv("HAS_GPU", "NO")=="YES":
-            self.device = 0
-        else:
-            self.device = "mps"
+        self.data_yaml = data_yaml
+        self.yolo_dir = yolo_dir
+        self.device = device
 
         os.makedirs(self.yolo_dir, exist_ok=True)
 
 
-    def train_model(self, epochs):
+    def train_model(self, epochs: int) -> None:
+        """
+        Trains the model configured in __init__.
+        Logs the results in yolo_dir (unused; you can delete this freely) and in mlflow for latter retrieval.
+
+        Args:
+            epochs (int): The number of epochs of training.
+        """
         with mlflow.start_run(run_name=self.model_name, log_system_metrics=True) as run:
 
             model = YOLO(model=self.model_name+".pt")
@@ -53,8 +66,12 @@ class Train():
             )
 
 
-    def register_model(self):
-
+    def register_model(self) -> None:
+        """
+        Registers the model and assignsn an alias.
+        The alias can either be Champion if there is no earlier run of the model or Challenger if it performs better than the current Champion.
+        An alias is unique in mlflow. The challenger alias refers to the last challenger. All the challenger models are identified via the "status" tag
+        """
         run_id = mlflow.last_active_run().info.run_id
 
         model_version = mlflow.register_model(
@@ -75,6 +92,9 @@ class Train():
             client.set_registered_model_alias(
                 name=self.model_name, alias="Champion", version=model_version.version
             )
+            client.set_registered_model_tag(
+                name=self.model_name, key="status", value="Champion"
+            )
         else:
             KEY_METRIC = "metrics/mAP50-95B"
             current_metrics = mlflow.get_run(run_id).data.metrics
@@ -86,12 +106,30 @@ class Train():
                 client.set_registered_model_alias(
                     name=self.model_name, alias="Challenger", version=model_version.version
                 )
+                client.set_registered_model_tag(
+                    name=self.model_name, key="status", value="Challenger"
+                )
             else:
                 console.print("[green]Current model is worse than champion: not setting alias[/]")
 
 
 if __name__ == "__main__":
-    epochs = 2 #in env
-    train = Train("yolo11n")
+
+    data_yaml = os.path.join(os.getcwd(), os.getenv("DATA_YAML", "data/THE-dataset/yolo.yaml"))
+    yolo_dir = os.path.join(os.getcwd(),  os.getenv("YOLO_DIR_TMP", "tmp/yolo_runs"))
+    try:
+        device = os.getenv("DEVICE")
+    except:
+        device = "cpu"
+    epochs = int(os.getenv("EPOCHS", 2))
+    model_name = os.getenv("MODEL", "yolo11n")
+
+    train = Train(
+        model_name=model_name,
+        data_yaml=data_yaml,
+        yolo_dir=yolo_dir,
+        device=device
+        )
+    
     train.train_model(epochs)
     train.register_model()
